6df51c2aca599e591304da8faba85b686a3e89c3
manishreddyjava <manishreddypatkuri@gmail.com>
2020-08-02 18:57:14 -0400

    initial commit

--- src/emailapp/Emailapp.java
+++ src/emailapp/Emailapp.java
@@ -4,6 +4,7 @@ public class Emailapp {
     public static void main(String[]args){
         Email em1=new Email("john","smith");
         System.out.println("push to github");
+        System.out.println("new branch created");
 
     }
 }
